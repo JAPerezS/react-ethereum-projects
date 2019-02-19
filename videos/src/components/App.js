f445e198f757fd1ab8ed80f1643e574303d6c3f4
JAPerezS <jose_ps0@hotmail.com>
2019-02-18 23:36:02 -0600

    up to video 110

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -1,8 +1,13 @@
 import React, { Component } from "react";
+import SearchBar from "./SearchBar";
 
 class App extends Component {
   render() {
-    return <div>Hello world!</div>;
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
   }
 }
 
