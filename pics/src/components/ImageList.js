00afcf9bd9e62f51defef3dadc5995ab6259aee9
JAPerezS <jose_ps0@hotmail.com>
2019-02-18 23:32:47 -0600

    up to 109

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,6 +1,6 @@
-import "./ImageList.css";
-import React from "react";
-import ImageCard from "./ImageCard";
+import './ImageList.css';
+import React from 'react';
+import ImageCard from './ImageCard';
 
 const ImageList = props => {
   const images = props.images.map(image => {
